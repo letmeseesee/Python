188030edf9b16c4618d580b9c9bac28e1080b886
yhwang <1903417374@qq.com>
2017-07-11 21:18:35 +0800

    0711

---
+++ model_shutil.py
@@ -0,0 +1,9 @@
+#Created by yhwang on 2017/7/11.
+import shutil
+#拷贝文件
+f_source = open('a.txt','w',encoding='utf-8')
+f_target = open('b.txt','w',encoding='utf-8')
+shutil.copyfileobj(f_source,f_target)#copyfile可以直接输入文件地址而不用打开
+
+#copymode拷贝权限
+#shutil.copymode()
--- second.py
+++ second.py
@@ -26,3 +26,78 @@ def change_name():
 change_name()
 
 print(name,name1)
+
+#装饰器--->本质是一个函数,用来装饰其他函数,为其他函数添加额外功能
+#装饰器原则:1.不能修改被装饰的函数的源代码
+#         2.不能修改被装饰的函数的调用方式
+#实现装饰器的知识储备
+#         1.函数是变量 2.高阶函数 3.函数嵌套
+#           2+3=>装饰器
+import time
+
+#一个装饰器
+def timer(func):
+    def wapper(*args,**kwargs):
+        func()
+        print(123)
+    return wapper
+
+@timer
+def test_d():
+    time.sleep(3)
+
+#怎么解决参数的问题???
+def mydeco(func):
+    def deco(*args,**kwargs):#在这里加上参数-->同时解决了多个函数参数个数不同的问题
+        print("In deco")
+        res = func(*args,**kwargs)#如果function有结果的话
+        return res
+    return deco()
+
+#装饰器带参数的情况---->封装最深的情况
+def deepdeco(type):
+    print(type)
+    def mydeco(func):
+        def deco(*args, **kwargs):  # 在这里加上参数-->同时解决了多个函数参数个数不同的问题
+            print("In deco")
+            res = func(*args, **kwargs)  # 如果function有结果的话
+            return res
+
+        return deco()
+    return mydeco
+#用户限制验证装饰器功能
+def auth(func):
+    def deco(*args,**kwargs):
+        #登录验证
+        #可以加上从终端输入
+        func(*args,**kwargs)
+@deepdeco(type=1)
+def test_deep():
+    print(123)
+test_d()
+
+
+#生成器就是一边循环一边计算的意思
+#-----------------生成式----------------
+print("-----------------生成式----------------")
+a = [x for x in range(10) if x<5]
+b = (x for x in range(100000000))#生成器只有在调用时才会生成相应的数据
+print(b)
+print(b.__next__)
+print(b.__next__())
+
+#写一个生成器---->带yield就行
+def fib(max):
+    a=0
+    b=1
+    n=0
+    while n<max:
+        a,b = b,a+b
+        n = n+1
+        yield b
+
+for i in fib(10):
+    print(i)
+
+f = fib(10)
+print(f.__next__())
